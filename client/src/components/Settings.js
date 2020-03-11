e6a54f31677f37e5c62aee80d09da8741e5a542e
Josh <joshvelasqez6@gmail.com>
2020-03-10 20:11:24 -0600

    Fixed extra html tag

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -3,20 +3,18 @@ import React from "react";
 class Settings extends React.Component {
   render() {
     return (
-      <h2 class="ui header">
-        <div class="content">
-          <div class="ui compact menu">
-            <div class="ui simple dropdown item">
-              <i class="settings icon small"></i>
-              <div class="menu">
-                <div class="item">Dark Mode</div>
-                <div class="item">Payment</div>
-                <div class="item">Messaging</div>
-              </div>
+      <div class="content">
+        <div class="ui compact menu">
+          <div class="ui simple dropdown item">
+            <i class="settings icon small"></i>
+            <div class="menu">
+              <div class="item">Dark Mode</div>
+              <div class="item">Payment</div>
+              <div class="item">Messaging</div>
             </div>
           </div>
         </div>
-      </h2>
+      </div>
     );
   }
 }
