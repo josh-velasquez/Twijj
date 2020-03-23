0c077214c6e5cee3343036540a74c2d2d029c29c
unknown <jeffhuo77@gmail.com>
2020-03-23 16:35:46 -0600

    Formatting tabs and spaces

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -5,44 +5,45 @@ import OpenPNG from "./open.png"
 import HolderPNG from "./holder.png"
 
 class SideNav extends React.Component {
-	    constructor(props) {
-        super(props)
-        this.state = {
-            subscriptions: [],
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      subscriptions: [],
     }
-	
-retrieveSubscribers(){
-	/*replace this code with actual code to populate subcriptions state*/
-	this.setState(state => ({
-		subscriptions: [HolderPNG,HolderPNG],
-	}))
-}
+  }
+    
+  retrieveSubscribers(){
+    /*replace this code with actual code to populate subcriptions state*/
+    this.setState(state => ({
+      subscriptions: [HolderPNG,HolderPNG],
+    }))
+  }
 
-componentDidMount(){
-	this.retrieveSubscribers();
-}
+  componentDidMount(){
+    this.retrieveSubscribers();
+  }
 
-displaySubscribers(){
-	let arr = [];
-	for(let i = 0; i < this.state.subscriptions.length; i++){
-		arr.push(<div className="subIcon" key={i}><img src={this.state.subscriptions[i]} style={{width: "50px" , height: "50px"}}></img></div>);
-	}
-	return arr;
-}
+  displaySubscribers(){
+    let arr = [];
+    for(let i = 0; i < this.state.subscriptions.length; i++){
+        arr.push(<div className="subIcon" key={i}><img src={this.state.subscriptions[i]} style={{width: "50px" , height: "50px"}}></img></div>);
+    }
+    return arr;
+  }
 
-render() {
-  return (
-	<div class="sidenav">
-		<div className="subIcon">
-			<span>
-				<img src={OpenPNG} style={{width: "50px" , height: "50px"}}></img>
-			</span>
-		</div>
-		{this.displaySubscribers()}
-	</div>
-  )
-}
+  render() {
+    return (
+      <div class="sidenav">
+        <div className="subIcon">
+          <span>
+            <img src={OpenPNG} style={{width: "50px" , height: "50px"}}></img>
+          </span>
+        </div>
+        {this.displaySubscribers()}
+      </div>
+    )
+  }
+
 };
 
 export default SideNav;
