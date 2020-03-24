2567ed966d8590ce1ffe2bb039e98f5fa020a17d
Josh Velasquez <joshuavelasquez@Joshs-MacBook-Pro.local>
2020-03-23 21:06:28 -0600

    Added new line in script

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -29,7 +29,7 @@ class StreamShow extends React.Component {
     const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
-      url: `http://localhost:8000/live/${id}.flv`
+      url: `http://192.168.1.78:8000/live/${id}.flv`
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
