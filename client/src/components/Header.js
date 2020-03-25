355f0fba1c4116cfe5d864173d1b950c577d1867
Josh <joshvelasqez6@gmail.com>
2020-03-24 19:30:13 -0600

    Fixed nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,30 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Profile from "./profile/Profile";
 import GoogleAuth from "./GoogleAuth";
-import Settings from "./Settings";
 
 import "./style.css";
-import Logo from "./logo.png";
+import Logo from "./logonobg.png";
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu" style={{backgroundColor: "black"}}>
-      <div id="logo">
-      <Link to="/">
-          <img src={Logo} style={{width: "200px" , height: "50px"}}></img>
-      </Link>
+    <div class="ui menu inverted">
+      <div class="header item">
+        <Link to="/">
+          <img src={Logo} style={{ width: "100px" }}></img>
+        </Link>
       </div>
-        <Link style={{color: "white"}} className="item">Stream</Link>
-        <Link style={{color: "white"}} className="item">Browse</Link>
-        <div className="right menu">
-          <div className="item">
-            <div className="ui icon input">
-              <input type="text" placeholder="Search..."/>
-              <i className="search link icon"></i>
+      <a class="item active">Streams</a>
+      <a class="item">Browse</a>
+      <div className="right menu">
+        <div className="item">
+          <div className="ui icon input">
+            <input type="text" placeholder="Search..." />
+            <i className="search link icon"></i>
+          </div>
+        </div>
+        <div className="item">
+          <GoogleAuth />
+        </div>
+        <div className="item">
+          <div class="ui compact menu">
+            <div class="ui simple dropdown item">
+              <i class="settings icon small"></i>
+              <div class="menu">
+                <div class="item">Dark Mode</div>
+                <div class="item">Payment</div>
+                <div class="item">Messaging</div>
+              </div>
             </div>
           </div>
-          <GoogleAuth />
-          <Settings />
+        </div>
       </div>
     </div>
   );
