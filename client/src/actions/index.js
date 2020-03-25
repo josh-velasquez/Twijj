ce87fab0ce46361b0cafaa05cf4754fc3aae5855
Josh Velasquez <joshuavelasquez@Joshs-MacBook-Pro.local>
2020-03-24 18:47:28 -0600

    Changing deploy path to build instead of dist. Change permissions of script

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -25,6 +25,7 @@ export const signOut = () => {
 
 export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
+  console.log("WENT HERE");
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
   history.push("/");
