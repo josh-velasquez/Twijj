2e3de0ac68213d6c9f63e08101b50a1b6239275e
Josh <joshvelasqez6@gmail.com>
2020-04-03 17:46:18 -0600

    Updated gear icon on header and side nav bar

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,32 +1,39 @@
 import React from "react";
 import "./style.css";
 
-import OpenPNG from "./open.png"
-import HolderPNG from "./holder.png"
+import OpenPNG from "./open.png";
+import HolderPNG from "./holder.png";
 
 class SideNav extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      subscriptions: [],
-    }
+      subscriptions: []
+    };
   }
 
-  retrieveSubscribers(){
+  retrieveSubscribers() {
     /*replace this code with actual code to populate subcriptions state*/
     this.setState(state => ({
-      subscriptions: [HolderPNG,HolderPNG],
-    }))
+      subscriptions: [HolderPNG, HolderPNG]
+    }));
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.retrieveSubscribers();
   }
 
-  displaySubscribers(){
+  displaySubscribers() {
     let arr = [];
-    for(let i = 0; i < this.state.subscriptions.length; i++){
-        arr.push(<div className="subIcon" key={i}><img src={this.state.subscriptions[i]} style={{width: "50px" , height: "50px"}}></img></div>);
+    for (let i = 0; i < this.state.subscriptions.length; i++) {
+      arr.push(
+        <div className="subIcon" key={i}>
+          <img
+            src={this.state.subscriptions[i]}
+            style={{ width: "20px", height: "20px" }}
+          ></img>
+        </div>
+      );
     }
     return arr;
   }
@@ -36,14 +43,13 @@ class SideNav extends React.Component {
       <div className="sidenav">
         <div className="subIcon">
           <span>
-            <img src={OpenPNG} style={{width: "50px" , height: "50px"}}></img>
+            <i class="bars large icon"></i>
           </span>
         </div>
         {this.displaySubscribers()}
       </div>
-    )
+    );
   }
-
-};
+}
 
 export default SideNav;
